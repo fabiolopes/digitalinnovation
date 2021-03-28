5399caaa3c710ad3f880abd41629c119dfdea87b
Fabio Bione <fabiolopesbione@hotmail.com>
2021-03-28 00:05:46 -0300

    enums

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,10 +4,13 @@ class Pessoa {
     var nome: String = "Bione"
     var cpf: String = "123.123.123-01"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val bione = Pessoa()
-    println(bione.nome)
-    println(bione.cpf)
+    println(bione.pessoaInfo())
 }
